6f05e7d03b0445b72f9fdca6fb06fe120066c063
zeuse <zeuserosmardel@hotmail.com.ar>
2024-03-06 19:34:27 -0300

    Primer commit

---
+++ src/main/java/com/example/jwt_practica/JwtPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPracticaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtPracticaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jwt_practica/JwtPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracticaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
