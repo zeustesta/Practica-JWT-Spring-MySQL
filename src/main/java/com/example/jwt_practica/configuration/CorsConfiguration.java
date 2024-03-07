6c402d002ab68bf8f5fb61c6a932d6c1942bddbd
zeuse <zeuserosmardel@hotmail.com.ar>
2024-03-07 13:01:23 -0300

    Dependencies added and security configuration started

---
+++ src/main/java/com/example/jwt_practica/configuration/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.jwt_practica.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+  private static final String GET = "GET";
+  private static final String POST = "POST";
+  private static final String DELETE = "DELETE";
+  private static final String PUT = "PUT";
+  public WebMvcConfigurer CorsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedMethods(GET, POST, PUT, DELETE)
+          .allowedHeaders("*")
+          .allowedOriginPatterns("*")
+          .allowCredentials(true);
+      }
+    };
+  }
+}
---
+++ src/main/java/com/example/jwt_practica/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.example.jwt_practica.configuration;
+
+public class WebSecurityConfiguration {
+}
